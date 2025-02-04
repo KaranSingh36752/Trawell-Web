ad81eb71a5431b843a416954339328e510e45fc2
KaranSingh36752 <karansingh36752@gmail.com>
2025-02-04 17:01:18 +0530

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utilis/contants";
+import { useDispatch, useSelector } from "react-redux";
+import { setFeed } from "../utilis/feedSlice";
 
 const Feed = () => {
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
+  const getfeed = async () => {
+    const res = await axios.get(`${BASE_URL}/user/feed?limit=5`, { withCredentials: true });
+    console.log(res.data);
+    dispatch(setFeed(res.data));
+  };
+
+  useEffect(() => {
+    getfeed();
+  }, []);
+
   return (
     <>
-     <span>FEDDDDDDDDDEDEDEDEED</span>
+      <span>FEDDDDDDDDDEDEDEDEED</span>
     </>
-    
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;

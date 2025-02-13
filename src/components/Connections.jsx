1d93e32179df14f8e0c2cadf3f8e1c2ab34579b9
KaranSingh36752 <karansingh36752@gmail.com>
2025-02-13 16:35:20 +0530

    connection fetch

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utils/contants";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnection } from "../utils/connectionsSlice";
 
 const Connections = () => {
-  return (
-    <div className='bg-black'>
-        <div>
-            <h1 className='text-white'>Connections</h1>
-        </div>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const connections = useSelector((store) => store.connections);
 
-export default Connections
+  const fetchConnection = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/user/connections`, {
+        withCredentials: true,
+      });
+      console.log(res?.data?.data);
+      dispatch(addConnection(res?.data?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnection();
+  }, []);
+
+  if (!connections) return null;
+
+  if (connections.length === 0) return <h1>NO Connections Found</h1>;
+
+  return (
+    <div className="text-center">
+      <h1 className="text-black text-2xl font-bold">Connections</h1>
+      <div>
+        {connections.map((connection) => (
+          <div key={connection._id}>
+            <h1>{connection.firstName + " " + connection.lastName}</h1> 
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Connections;

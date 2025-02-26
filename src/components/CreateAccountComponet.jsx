03f5c5f18cd33a928f6227d02ad9828fbe2fae7e
KaranSingh36752 <karansingh36752@gmail.com>
2025-02-26 20:01:57 +0530

    signup

--- src/components/CreateAccountComponet.jsx
+++ src/components/CreateAccountComponet.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-import { Mail, KeyRound } from "lucide-react";
+import { Mail, KeyRound, User } from "lucide-react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
@@ -8,12 +9,16 @@ import { useNavigate } from "react-router";
 import { BASE_URL } from "../utils/contants";
 
 const CreateAccountComponent = ({ onClose }) => {
-  const [emailId, setEmailId] = useState("gauri@gmail.com");
-  const [password, setPassword] = useState("25Oct2004@");
-  const [error , setError] = useState("");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [error, setError] = useState("");
   const modalRef = useRef();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [signUpToggle, setSignUpToggle] = useState(false);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -30,15 +35,19 @@ const CreateAccountComponent = ({ onClose }) => {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post(`${BASE_URL}/login`, {
-        emailId,
-        password,
-      },{withCredentials: true});
+      const res = await axios.post(
+        `${BASE_URL}/login`,
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
       // console.log(res.data);
       dispatch(addUser(res.data));
       return navigate("/feed");
     } catch (err) {
-      setError(err?.response?.data)
+      setError(err?.response?.data);
     }
   };
 
@@ -51,75 +60,125 @@ const CreateAccountComponent = ({ onClose }) => {
     >
       <motion.div
         ref={modalRef}
-        className="bg-white rounded-lg shadow-xl w-full max-w-md p-6"
+        className="bg-white rounded-lg shadow-xl w-full max-w-md p-6 z-10 mt-5"
         initial={{ scale: 0.8 }}
         animate={{ scale: 1 }}
         exit={{ scale: 0.8 }}
       >
         <div className="flex justify-center items-center mb-4">
           <h2 className="text-xl font-bold flex justify-center">
-            Create Account
+            Welcome to Trawell
           </h2>
         </div>
         <div className="space-y-4">
-          <button className="w-full flex items-center justify-center space-x-2 bg-blue-500 text-white px-4 py-2 rounded-lg">
-            <span>Continue with Google</span>
-          </button>
-          <button className="w-full flex items-center justify-center space-x-2 bg-blue-600 text-white px-4 py-2 rounded-lg">
-            <span>Log in with Facebook</span>
-          </button>
-          <div className="flex items-center">
-            <div className="flex-1 border-t border-gray-300"></div>
-            <span className="mx-4 text-gray-500">or</span>
-            <div className="flex-1 border-t border-gray-300"></div>
-          </div>
           <div className="space-y-2">
-            <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
-              <Mail className="w-5 h-5 text-gray-500" />
-              <input
-                type="tel"
-                placeholder="Email"
-                className="flex-1 outline-none"
-                value={emailId}
-                onChange={(e) => setEmailId(e.target.value)}
-              />
+            {signUpToggle && (
+              <>
+                <div>
+                  <label className="text-black font-semibold mx-3">
+                    First Name
+                  </label>
+                  <div className="w-auto flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
+                    <User className="w-5 h-5 text-gray-500" />
+                    <input
+                      type="tel"
+                      placeholder="Tyler"
+                      className="flex-1 outline-none"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="text-black font-semibold mx-3">
+                    Last Name
+                  </label>
+                  <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
+                    <User className="w-5 h-5 text-gray-500" />
+                    <input
+                      type="tel"
+                      placeholder="Durden"
+                      className="flex-1 outline-none"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </>
+            )}
+            <div>
+              <label className="text-black font-semibold mx-3">
+                Email Address
+              </label>
+              <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
+                <Mail className="w-5 h-5 text-gray-500" />
+                <input
+                  type="tel"
+                  placeholder="Hellskitchen25@gmail.com"
+                  className="flex-1 outline-none"
+                  value={emailId}
+                  onChange={(e) => setEmailId(e.target.value)}
+                />
+              </div>
             </div>
             {/* <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
               <Smartphone className="w-5 h-5 text-gray-500" />
               <input type="tel" placeholder="Phone number" className="flex-1 outline-none" />
             </div> */}
-            <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
-              <KeyRound className="w-5 h-5 text-gray-500" />
-              <input
-                type="tel"
-                placeholder="Password"
-                className="flex-1 outline-none"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            <div>
+              <label className="text-black font-semibold mx-3">Password</label>
+              <div className="flex items-center space-x-2 border border-gray-300 rounded-lg px-4 py-2">
+                <KeyRound className="w-5 h-5 text-gray-500" />
+                <input
+                  type="tel"
+                  placeholder="Password"
+                  className="flex-1 outline-none"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
             </div>
             <div className="text-red-500 font-bold text-sm ml-2">
               <span>{error}</span>
             </div>
           </div>
-          <button className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={handleLogin}>
+          <button
+            className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg"
+            onClick={handleLogin}
+          >
             Continue
           </button>
+          <div>
+            <h1 className="text-black text-sm ml-3 -mt-2">
+              Don't have an account?{" "}
+              <span
+                className=" cursor-pointer underline text-blue-500"
+                onClick={() => setSignUpToggle((value) => !value)}
+              >
+                Sign up
+              </span>
+            </h1>
+          </div>
+          <div className="flex items-center">
+            <div className="flex-1 border-t border-gray-300"></div>
+            <span className="mx-4 text-gray-500">or</span>
+            <div className="flex-1 border-t border-gray-300"></div>
+          </div>
+          <div className=" flex justify-center gap-3">
+            <button className="text-2xl text-black ">
+              <FaGithub />
+            </button>
+            <button className="text-black text-2xl">
+              <FaGoogle />
+            </button>
+          </div>
           <p className="text-center text-sm text-gray-600">
             By tapping Log in or Continue, you agree to our Terms. Learn how we
             process your data in our Privacy Policy, and Cookie Policy.
           </p>
           <div className="text-center">
             <a href="#" className="text-sm text-blue-500 hover:underline">
-              Trouble Logging In?
-            </a>
-          </div>
-          <div className="flex justify-center space-x-4">
-            <a href="#" className="flex items-center space-x-2 text-gray-700">
-              <span>App Store</span>
-            </a>
-            <a href="#" className="flex items-center space-x-2 text-gray-700">
-              <span>Google Play</span>
+              Trouble Logging In? Forget password
             </a>
           </div>
         </div>
